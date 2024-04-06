50ad0bfedecad4b45c1ed00ada740e963a53072e
Jules <julesgrange@outlook.fr>
2024-04-06 16:27:34 +0200

    test commit

--- src/fr/codex/naturalis/Main.java
+++ src/fr/codex/naturalis/Main.java
@@ -3,6 +3,7 @@ package fr.codex.naturalis;
 public class Main {
     public static void main(String[] args) {
         var game = new Game(300);
+        System.out.println("test");
         game.start();
     }
 }
