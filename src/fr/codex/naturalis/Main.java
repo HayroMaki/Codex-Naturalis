c33ea77a1e382d24499f1d2969b3dbe67091aad0
Jules <julesgrange@outlook.fr>
2024-04-06 17:02:28 +0200

    added a RessourceDrawingSequence class to draw the ressource icons in the future.

--- src/fr/codex/naturalis/Main.java
+++ src/fr/codex/naturalis/Main.java
@@ -3,7 +3,6 @@ package fr.codex.naturalis;
 public class Main {
     public static void main(String[] args) {
         var game = new Game(300);
-        System.out.println("test");
         game.start();
     }
 }
--- src/fr/codex/naturalis/drawing/RessourceDrawingSequence.java
+++ src/fr/codex/naturalis/drawing/RessourceDrawingSequence.java
@@ -1,4 +1,23 @@
 package fr.codex.naturalis.drawing;
 
+import fr.codex.naturalis.corner.Ressource;
+
+import java.awt.*;
+import java.util.Objects;
+
 public class RessourceDrawingSequence {
+    private final Graphics2D graphics2D;
+    int width;
+    int height;
+    int cornerSize;
+    public Ressource ressource;
+    public RessourceDrawingSequence(Graphics2D graphics2D, int width, Ressource ressource) {
+        Objects.requireNonNull(graphics2D);
+        Objects.requireNonNull(ressource);
+        this.graphics2D = graphics2D;
+        this.ressource = ressource;
+        this.width = width;
+        height = width / 2;
+        cornerSize = (height / 2) - (height / 10);
+    }
 }
